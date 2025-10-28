995f46fa4ea7e283afc8d90739ca2758c3b5794e
VASANTH109749 <147709153+VASANTH109749@users.noreply.github.com>
2025-10-28 16:52:39 +0530

    Add files via upload

---
+++ task-4.py
@@ -0,0 +1,28 @@
+name ="VASANTH"
+print(name)
+age = 25
+print(age)
+height = 175.5
+print(height)
+weight = 80
+print(weight)
+city = "Madurai"
+print(city)
+is_student = False
+print(is_student)
+marks =95
+print(marks)
+pi=3.14
+print(pi)
+school_name= "Scopetech software solution"
+print(school_name)
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+my_number=10
+print(my_number)
+print(type(my_number))
+my_text="Hello"
+print(my_text)
+print(type(my_text))
